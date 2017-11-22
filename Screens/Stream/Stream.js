e25801ace1af92374181687124920e7cb87f7e3a
v-ruban <v.ru6an@gmail.com>
2017-11-23 01:11:06 +0200

    stream sizing

--- Screens/Stream/Stream.js
+++ Screens/Stream/Stream.js
@@ -28,6 +28,19 @@ export default class Stream extends React.Component {
     const {navigate} = this.props.navigation;
     let {width, height} = Dimensions.get('screen');
     let headerStyle = this.state.isLandscape ? {height: 0} : {};
+
+    let topBarHeight = 24;
+    let buttonBarHeight = 48;
+    let headerHeight = 56;
+
+    let streamStyle = {
+      height: this.state.isLandscape ? height - topBarHeight : height/3,
+      width: this.state.isLandscape ? width - buttonBarHeight : width
+    };
+    let chatStyle = {
+      height: this.state.isLandscape ? 0 : (height * 2/3) - topBarHeight - buttonBarHeight - headerHeight,
+      width: width
+    };
     return (
       <Layout navigate={navigate} isCollapsed={this.state.isLandscape}>
         <Header style={headerStyle}>
@@ -45,12 +58,12 @@ export default class Stream extends React.Component {
         <WebView
           source={{uri: this.state.streamLink}}
           allowfullscreen="false"
-          style={{width: width, height: this.state.isLandscape ? height : height/3}}
+          style={streamStyle}
           height={200}
         />
         <WebView
           source={{uri: this.state.chatLink}}
-          style={{width: width, height: this.state.isLandscape ? 0 : height * 2/3 - 175}}
+          style={chatStyle}
         />
       </Layout>
     );
