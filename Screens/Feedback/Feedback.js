968a1b9cf2d8e99cf8c9f35a1c73e40181e3bdaa
v-ruban <v.ru6an@gmail.com>
2017-11-23 02:24:45 +0200

    dummy feedback form

--- Screens/Feedback/Feedback.js
+++ Screens/Feedback/Feedback.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Body, Button, Card, CardItem, Col, Container, Content, Footer, FooterTab, Grid, H1, H3, Header, Icon, Input, Item, Left, List, ListItem, Right, Spinner, Text, Thumbnail, View, Tab, Tabs, TabHeading, Separator, Row, Title } from "native-base";
+import { Body, Button, Card, CardItem, Col, Container, Content, Footer, FooterTab, Grid, H1, H3, Header, Icon, Input, Item, Left, List, ListItem, Right, Spinner, Text, Thumbnail, View, Tab, Tabs, TabHeading, Separator, Row, Title, Form, Label } from "native-base";
 import Layout from '../Layout';
 
 export default class Feedback extends React.Component {
@@ -23,9 +23,25 @@ export default class Feedback extends React.Component {
           </Body>
           <Right />
         </Header>
-        <Body>
-        <Text>Feedback</Text>
-        </Body>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Email</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Name</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Message</Label>
+              <Input />
+            </Item>
+          </Form>
+          <Button block style={{marginLeft: 25, marginRight: 25, marginTop: 25}}>
+            <Text>Send feedback</Text>
+          </Button>
+        </Content>
       </Layout>
     );
   }
